4d18f002c93592ef87407da799ceac469435eb40
Bpost129 <Bpost129@yahoo.com>
2025-10-30 14:40:05 -0400

    select input from dom, set new comment text to input value, clear input when creating new comment

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 const likeButton = document.querySelector('#like-button')
 const commentButton = document.querySelector('#comment-button')
 const commentList = document.querySelector('ul')
+const inputElement = document.querySelector('input')
 
-
+console.dir(inputElement)
 
 likeButton.addEventListener('click', () => {
   console.log('You clicked me!')
 })
 
 commentButton.addEventListener('click', () => {
-  const commentElement = document.createElement('li')
-  commentElement.textContent = 'Can you hear me?'
-  commentList.appendChild(commentElement)
+  if (inputElement.value) {
+    const commentElement = document.createElement('li')
+    commentElement.textContent = inputElement.value
+    commentList.appendChild(commentElement)
+    inputElement.value = ''
+  }
 })
