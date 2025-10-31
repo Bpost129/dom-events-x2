ce275e5dcb15339e0292204b09729fe94710c4ad
Bpost129 <Bpost129@yahoo.com>
2025-10-31 11:43:22 -0400

    add event listener to buttons div

--- js/app.js
+++ js/app.js
@@ -5,16 +5,37 @@ const commentList = document.querySelector('ul')
 const inputElement = document.querySelector('input')
 const likesCountElement = document.querySelector('#likes-count')
 
+
+const bodyElement = document.querySelector('body')
+const divElement = document.querySelector('div')
+
+bodyElement.addEventListener('click', () => {
+  console.log('body')
+})
+
+divElement.addEventListener('click', () => {
+  console.log('div')
+})
+
+
 let likesCount = 0
 
 const handleReaction = (e) => {
-  e.target.id === 'like-button' ? likesCount += 1 : likesCount -= 1
-  likesCountElement.textContent = likesCount
-  console.log(likesCount)
+  console.log(e.target.id)
+  if (e.target.id === 'like-button') {
+    likesCount += 1 
+    likesCountElement.textContent = likesCount
+  } 
+  if (e.target.id === 'dislike-button') {
+    likesCount -= 1 
+    likesCountElement.textContent = likesCount
+  }
+  e.stopPropagation()
 }
 
-likeButton.addEventListener('click', handleReaction)
-dislikeButton.addEventListener('click', handleReaction)
+divElement.addEventListener('click', handleReaction)
+// likeButton.addEventListener('click', handleReaction)
+// dislikeButton.addEventListener('click', handleReaction)
 
 commentButton.addEventListener('click', () => {
   if (inputElement.value) {
